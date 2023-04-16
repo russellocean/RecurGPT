849d3e3317487690e3596aa9f116d53567b1e5fa
russellocean <russellwelch17@gmail.com>
2023-04-15 22:45:22 -0400

    rewrite ish

--- main.py
+++ main.py
@@ -1,42 +1,48 @@
-import os
-from typing import Optional
 
+from pyexpat import model
 from dotenv import load_dotenv
-import chardet
-import fnmatch
 
 from langchain import OpenAI
-from langchain.docstore.document import Document
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.docstore import InMemoryDocstore
 
-from agent import BabyAGI
-from agent_utils import ask_agent
+from langchain.agents import ZeroShotAgent, AgentExecutor
+from langchain import OpenAI, LLMChain
+from langchain.chat_models import ChatOpenAI
 
-from config import config
-from utils import ask_user_for_project
+
+
+
+from agent_tools import ListFilesAndDirectoriesTool, ViewCodeFilesTool, CreateFileTool, ModifyFileTool
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Define your embedding model
-embeddings_model = OpenAIEmbeddings()
+list_files_and_directories_tool = ListFilesAndDirectoriesTool()
+view_code_files_tool = ViewCodeFilesTool()
+create_file_tool = CreateFileTool()
+modify_file_tool = ModifyFileTool()
 
-# Initialize the vectorstore as empty
-import faiss
-
-embedding_size = 1536
-index = faiss.IndexFlatL2(embedding_size)
-vectorstore = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
-
-
-# Load environment variables
-load_dotenv()
 
 def main():
-    start_sequence()
+    #start_sequence()
+    tools = [list_files_and_directories_tool, view_code_files_tool, create_file_tool, modify_file_tool]
+    
+    prefix = """You are an AI that is tasked with the objective of {objective}.\n\
+    Your goal is to effectively coordinate the execution of tasks. Your responsibilities include:\n\
+    1. Creating new tasks based on the results of previous tasks.\n\
+    2. Prioritizing tasks based on the results of previous tasks.\n\
+    3. Executing tasks.\n\
+    Adhere to professional standards and best practices during the process.\n\
+    Always monitor your overall progress towards the objective."""
+
+
+    
+    suffix = """ 
+        Begin! 
+        
+        Objective: {objective}
+        {agent_scratchpad}
+    """
+    
     while True:
         question = input("What would you like the agent to do? (type 'quit' or 'exit' to close)\n")
         if question.lower() == 'quit' or question.lower() == 'exit':
@@ -44,79 +50,54 @@ def main():
         
         OBJECTIVE = question
         
-        llm = OpenAI(temperature=0)
+        prompt = ZeroShotAgent.create_prompt(
+            tools,
+            prefix = prefix,
+            suffix = suffix,
+            input_variables=["objective", "agent_scratchpad"]    
+        )
         
+        print(prompt.template)
+        
+        llm_chain = LLMChain(llm=OpenAI(temperature=0), prompt=prompt)
+        
+        llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+        tool_names = [tool.name for tool in tools]
+
+        agent = ZeroShotAgent(llm_chain=llm_chain, allowed_tools=tool_names, verbose=True, return_intermediate_steps=True)
+        agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
+        
+        response = agent_executor.run(OBJECTIVE)
+        
+        print(response)
+
         # Logging of LLMChains
         verbose=False
         # If None, will keep on going forever
-        max_iterations: Optional[int] = 3
-        baby_agi = BabyAGI.from_llm(
-            llm=llm,
-            vectorstore=vectorstore,
-            verbose=verbose,
-            max_iterations=max_iterations
-        )
-        
-        # Set up the agent_executor
-        baby_agi.agent_executor = BabyAGI.setup_agent(llm=llm)
-        
-        baby_agi({"objective": OBJECTIVE})
         
         
-def start_sequence():
-    print("Welcome to RecurGPT please select your project folder.")
+        
+        
+# def start_sequence():
+#     print("Welcome to RecurGPT please select your project folder.")
     
-    config.project_folder = ask_user_for_project()
+#     config.project_folder = ask_user_for_project()
     
-    # Read files from the directory
-    project_folder = config.project_folder
-    documents = read_files_from_directory(project_folder)
+#     # Read files from the directory
+#     project_folder = config.project_folder
+#     documents = read_files_from_directory(project_folder)
     
-    # Split documents using TextSplitter
-    #text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 1000,
-        chunk_overlap  = 50,
-    )
-    document_objs = [Document(page_content=text) for text in documents]
-    texts = text_splitter.split_documents(document_objs)
+#     # Split documents using TextSplitter
+#     #text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+#     text_splitter = RecursiveCharacterTextSplitter(
+#         chunk_size = 1000,
+#         chunk_overlap  = 50,
+#     )
+#     document_objs = [Document(page_content=text) for text in documents]
+#     texts = text_splitter.split_documents(document_objs)
     
-    print("Current project folder:" + config.project_folder)
+#     print("Current project folder:" + config.project_folder)
     
-# def chroma_search(query):
-#     results = chroma_store.similarity_search(query)
-#     # Process the results and return the desired output
-#     return results
-
-def read_gitignore(directory):
-    gitignore_path = os.path.join(directory, '.gitignore')
-    if os.path.exists(gitignore_path):
-        with open(gitignore_path, 'r', encoding='utf-8') as file:
-            return file.read().splitlines()
-    return []
-
-def is_ignored(file, gitignore):
-    for pattern in gitignore:
-        if fnmatch.fnmatch(file, pattern):
-            return True
-    return False
-
-def read_files_from_directory(directory):
-    documents = []
-    gitignore = read_gitignore(directory)
-    for filename in os.listdir(directory):
-        file_path = os.path.join(directory, filename)
-        print(f"Reading file: {file_path}")
-        if os.path.isfile(file_path) and not is_ignored(filename, gitignore):
-            with open(file_path, 'rb') as file:
-                raw_data = file.read()
-                encoding = chardet.detect(raw_data)['encoding']
-                print(f"Detected encoding: {encoding}")
-            with open(file_path, 'r', encoding=encoding) as file:
-                content = file.read()
-                documents.append(content)
-                print(f"Read {len(content)} characters from file: {file_path}")
-    return documents
 
 if __name__ == "__main__":
     main()
